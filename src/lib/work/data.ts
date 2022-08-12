fbab658da4d7ba2eefc7fe3db9ffb7f213288d7e
Alex Iglesias <alexiglesias@finsweet.com>
2022-08-12 20:45:52 +0200

    Completed work data

--- src/lib/work/data.ts
+++ src/lib/work/data.ts
@@ -7,8 +7,8 @@ export const WORK_ITEMS = [
     id: nanoid(),
     title: 'Finsweet Extension',
     description: 'A badass workflow toolkit for Webflow.',
-    href: 'https://www.finsweet.com/extension',
-    image: 'images/work/finsweet-extension.png',
+    href: 'https://chrome.google.com/webstore/detail/finsweet-extension-for-we/mjfibgdpclkaemogkfadpbdfoinnejep',
+    image: 'images/work/finsweet-extension.webp',
     tags: ['svelte', 'extension', 'typescript', 'webflow']
   },
   {
@@ -16,31 +16,39 @@ export const WORK_ITEMS = [
     title: 'Attributes',
     description: 'No-code Attributes solutions for Webflow.',
     href: 'https://www.finsweet.com/attributes',
-    image: 'images/work/attributes.png',
+    image: 'images/work/attributes.webp',
     tags: ['typescript', 'webflow']
+  },
+  {
+    id: nanoid(),
+    title: 'Finsweet SSO',
+    description: 'One single account for all Finsweet products, services, and preferences.',
+    href: 'https://www.finsweet.com/fin-pro/log-in-with-finsweet',
+    image: 'images/work/finsweet-sso.webp',
+    tags: ['svelte', 'typescript', 'backend']
   },
   {
     id: nanoid(),
     title: 'Cookie Consent',
     description: 'Incredibly powerful cookie consent plugin for Webflow.',
     href: 'https://www.finsweet.com/cookie-consent/',
-    image: 'images/work/cookie-consent.png',
+    image: 'images/work/cookie-consent.webp',
     tags: ['typescript', 'webflow']
   },
   {
     id: nanoid(),
-    title: 'Cookie Consent',
-    description: 'Incredibly powerful cookie consent plugin for Webflow.',
-    href: 'https://www.finsweet.com/cookie-consent/',
-    image: 'images/work/cookie-consent.png',
-    tags: ['typescript', 'webflow']
+    title: 'Enterprise Sales Demo',
+    description: 'Taking Webflow to the next level.',
+    href: 'https://www.finsweet.website/',
+    image: 'images/work/enterprise-sales-demo.webp',
+    tags: ['typescript', 'webflow', 'backend']
   },
   {
     id: nanoid(),
-    title: 'Finsweet Extension',
-    description: 'A badass workflow toolkit for Webflow.',
-    href: 'https://www.finsweet.com/extension',
-    image: 'images/work/finsweet-extension.png',
-    tags: ['svelte', 'extension', 'typescript', 'webflow']
+    title: 'Advanced Webflow Forms',
+    description: 'Multi steps & conditional logic for Webflow.',
+    href: 'https://advanced-forms.webflow.io/',
+    image: 'images/work/advanced-webflow-forms.webp',
+    tags: ['svelte', 'typescript', 'webflow']
   }
 ];
