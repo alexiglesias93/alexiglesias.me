75b1b9007cf557790fb77853eb0e426a2ba3f471
Alex Iglesias <alexiglesias@finsweet.com>
2022-08-31 11:25:10 +0200

    added +error layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<section class="section page-padding">
+  <div class="container">
+    <h1 class="mb-8">{$page.status}</h1>
+    <p>{$page.error.message}</p>
+  </div>
+</section>
