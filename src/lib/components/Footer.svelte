e7f1d13a27343f182a38b8b435433ce9c0b55b5e
Alex Iglesias <alexiglesias@finsweet.com>
2022-09-22 12:39:27 +0200

    Added RSS link in the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,9 +7,15 @@
     <div class="content">
       <Logo />
 
-      <a href="mailto:alexiglesias@finsweet.com?subject=Hello%20there!" class="text-link">
-        Say hello!
-      </a>
+      <div class="links">
+        <a data-sveltekit-reload href="/writing/rss.xml" target="_blank" class="text-link">RSS</a>
+
+        <div class="links-separator" />
+
+        <a href="mailto:alexiglesias@finsweet.com?subject=Hello%20there!" class="text-link">
+          Say hello!
+        </a>
+      </div>
     </div>
   </div>
 </footer>
@@ -20,5 +26,18 @@
     justify-content: space-between;
     align-items: center;
     font-size: 0.75rem;
+  }
+
+  .links {
+    display: flex;
+    align-items: center;
+    grid-column-gap: 0.5rem;
+  }
+
+  .links-separator {
+    width: 0.25rem;
+    height: 0.25rem;
+    border-radius: 999px;
+    background-color: var(--golden);
   }
 </style>
