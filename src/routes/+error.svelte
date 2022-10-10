2a88479eb2d7ee04d6480cdeca2c7963f84a38cc
Alex Iglesias <alexiglesias@finsweet.com>
2022-10-10 15:37:49 +0200

    chore: updated dependencies

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,6 +5,6 @@
 <section class="section page-padding">
   <div class="container">
     <h1 class="mb-8">{$page.status}</h1>
-    <p>{$page.error.message}</p>
+    <p>{$page.error?.message}</p>
   </div>
 </section>
